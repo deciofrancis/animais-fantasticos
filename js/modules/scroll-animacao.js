96a5751bfb88c531eac59fbbf7831409c93236b0
Décio Francis <francisdasilva@gmail.com>
2023-06-23 16:16:15 -0300

    organizando e modularizando codigo

---
+++ js/modules/scroll-animacao.js
@@ -0,0 +1,21 @@
+export default function initAnimacaoScroll() {
+    const section = document.querySelectorAll('[data-anime="scroll"]');
+
+    if(section.length) {
+        const windowMetade = window.innerHeight * 0.6;
+
+        function animaScroll() {
+            section.forEach((section) => {
+                const sectionTop = section.getBoundingClientRect().top;
+                const isSectionVisible = (sectionTop - windowMetade) < 0;
+                if(isSectionVisible) {
+                    section.classList.add('ativo');
+                }
+            })
+        }
+
+        animaScroll();
+
+        window.addEventListener('scroll', animaScroll);
+    }
+}
