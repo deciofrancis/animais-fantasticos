d200672fe7917b32cf30552be02bb4cd12b2b621
Décio Francis <francisdasilva@gmail.com>
2023-06-29 10:24:05 -0300

    adicionando funcionalidades ao modal com js

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ import initScrollSuave from './modules/scroll-suave.js';
 import initAnimacaoScroll from './modules/scroll-animacao.js';
 import initAccordion from './modules/accordionList.js';
 import initTavNav from './modules/tabnav.js';
+import initModal from './modules/modal.js';
 
 initScrollSuave();
 initAnimacaoScroll();
 initAccordion();
 initTavNav();
+initModal();
