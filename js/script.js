d7f469ec1f385fab0a7c9905c7afbb5fdc132eb3
Décio Francis <francisdasilva@gmail.com>
2023-06-30 12:08:37 -0300

    criando tooltip na area de map

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ import initAnimacaoScroll from './modules/scroll-animacao.js';
 import initAccordion from './modules/accordionList.js';
 import initTavNav from './modules/tabnav.js';
 import initModal from './modules/modal.js';
+import initTooltip from './modules/tooltip.js';
 
 initScrollSuave();
 initAnimacaoScroll();
 initAccordion();
 initTavNav();
 initModal();
+initTooltip();
