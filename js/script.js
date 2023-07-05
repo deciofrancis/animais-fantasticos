cdad9d065861b4e8dd6cc6bb8011a37f46aaae1f
Décio Francis <francisdasilva@gmail.com>
2023-07-04 23:47:36 -0300

    criando funcionalidas com js para o menu mobile

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ import initTavNav from './modules/tabnav.js';
 import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
+import initMenuMobile from './modules/menu-mobile.js';
 
 initScrollSuave();
 initAnimacaoScroll();
@@ -13,3 +14,4 @@ initTavNav();
 initModal();
 initTooltip();
 initDropdownMenu();
+initMenuMobile();
