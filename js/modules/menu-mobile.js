0ce761ca51170505f58d42c686b80ca3e5d78f9e
Décio Francis <francisdasilva@gmail.com>
2023-07-07 15:55:22 -0300

    criando funcao de observabilidade

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -1,4 +1,4 @@
-import outsideClick from './outsideClick.js';
+import outsideClick from "./outsideclick.js";
 
 export default function initMenuMobile() {
     const menuButton = document.querySelector('[data-menu="button"]');
@@ -16,5 +16,5 @@ export default function initMenuMobile() {
         }
     }
 
-    eventos.forEach(evento => menuButton.addEventListener('click', openMenu)); 
+    eventos.forEach(evento => menuButton.addEventListener(evento, openMenu)); 
 }
